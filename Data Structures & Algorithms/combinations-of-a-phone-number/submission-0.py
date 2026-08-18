99090178dda9b29bfc6fd4ea03c1b461064967c1
Ta Xuan Kien <kienkonvip@gmail.com>
2026-08-18 23:17:02 +0800

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+            
+        res = []
+        myMap = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+
+        def dfs(i, curr):
+            if i >= len(digits):
+                res.append(curr)
+                return
+            for char in myMap[digits[i]]:
+                dfs(i + 1, curr + char)
+        
+        dfs(0, "")
+        return res
