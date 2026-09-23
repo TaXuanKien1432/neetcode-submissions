0f509cd3a85a58c45969231435d06f76b0457a40
Ta Xuan Kien <kienkonvip@gmail.com>
2026-09-24 02:16:29 +0800

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prqMap = { i:[] for i in range(numCourses)}
+        for crs, prq in prerequisites:
+            prqMap[crs].append(prq)
+        
+        visited = set()
+
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if prqMap[crs] == []:
+                return True
+            
+            visited.add(crs)
+            for prq in prqMap[crs]:
+                if not dfs(prq):
+                    return False
+            visited.remove(crs)
+            prqMap[crs] = []
+            return True
+
+        for crs, prq in prerequisites:
+            if not dfs(crs):
+                return False
+        return True
+
